c30a22f60834b4afb59c7e18f8683c1b57b9bd5b
Pierre Favier <pierrefavier26@gmail.com>
2024-09-13 18:04:37 +0200

    enregistrement fichier

--- terre00.py
+++ terre00.py
@@ -1,1 +1,15 @@
-test
+"""
+Créez un programme qui affiche l’alphabet en lettres minuscules suivi d’un retour à la ligne.
+
+Exemples d’utilisation :
+$> python exo.py
+abcdefghijklmnopqrstuvwxyz
+$>
+
+Attention : votre programme devra utiliser une boucle.
+
+"""
+
+alphabet = "abcdefghijklmnopqrstuvwxyz\n"
+
+print (alphabet)
