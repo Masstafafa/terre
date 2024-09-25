2a119d442c5b348b1ed3d4114de5c8599e85ce4f
Pierre Favier <pierrefavier26@gmail.com>
2024-09-25 15:44:14 +0200

    modif

--- terre02.py
+++ terre02.py
@@ -1,13 +1,3 @@
-"""Créez un programme qui affiche les arguments qu’il reçoit ligne par ligne, peu importe le nombre d’arguments.
-
-
-Eemples d’utilisation :
-$> ruby exo.rb je suis solide !
-je
-suis
-solide
-!"""
-
 import sys
 
 sentence = sys.argv[1:]
