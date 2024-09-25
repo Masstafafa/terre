15f0f09ffb8b07b3eb34e39be16c181be25c2c9c
Pierre Favier <pierrefavier26@gmail.com>
2024-09-25 15:44:41 +0200

    modif

--- terre03.py
+++ terre03.py
@@ -1,14 +1,3 @@
-"""Créez un programme qui affiche l’alphabet à partir de la lettre donnée en argument en lettres minuscules suivi d’un retour à la ligne.
-
-
-Exemples d’utilisation :
-$> python exo.py n
-nopqrstuvwxyz
-$>
-
-
-Attention : votre programme devra utiliser une boucle."""
-
 import sys
 
 choice_letter = sys.argv[1]
