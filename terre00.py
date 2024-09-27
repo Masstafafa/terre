c2306b339131a8ce9a9abbbd4d3acb7b0c8b7a9d
Pierre Favier <pierrefavier26@gmail.com>
2024-09-27 11:07:13 +0200

    supression d'éléments

--- terre00.py
+++ terre00.py
@@ -1,18 +1,7 @@
-"""
-Créez un programme qui affiche l’alphabet en lettres minuscules suivi d’un retour à la ligne.
 
-Exemples d’utilisation :
-$> python exo.py
-abcdefghijklmnopqrstuvwxyz
-$>
-
-Attention : votre programme devra utiliser une boucle.
-
-"""
 #Valeur ascii a = 97 et z = 122. Faire en sorte de faire 
 #une boucle puis ensuite d'imprimer chacun des caractères
-a = 97
-z = 122
+
 for alphabet in range (97, 122 + 1):
     print(chr(alphabet), end="")
 print()
